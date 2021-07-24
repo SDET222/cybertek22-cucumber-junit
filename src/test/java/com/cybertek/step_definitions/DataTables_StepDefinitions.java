a4ca57600a6416b2a9c7e79bdd9b0dc69a8b5562
Orkhan <jkrogers222@gmail.com>
2021-07-24 11:52:11 -0400

    Drop down data table

--- src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/DataTables_StepDefinitions.java
@@ -1,5 +1,6 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.DropDownPage;
 import com.cybertek.pages.LibraryLoginPage;
 import com.cybertek.utilities.BrowserUtils1;
 import com.cybertek.utilities.ConfigurationReader;
@@ -8,13 +9,17 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class DataTables_StepDefinitions {
 
     LibraryLoginPage loginPage = new LibraryLoginPage();
+    DropDownPage dropDownPage = new DropDownPage();
 
     @Then("user should see below words displayed")
     public void user_should_see_below_words_displayed(List<String> listOfFruits) {
@@ -47,6 +52,27 @@ public class DataTables_StepDefinitions {
 
 
     }
+
+    @Given("User is on the dropdowns page of practice tool")
+    public void user_is_on_the_dropdowns_page_of_practice_tool() {
+        String dropdownPageUrl = ConfigurationReader.getProperty("dropdownUrl");
+       Driver.getDriver().get(dropdownPageUrl);
+    }
+    @Then("User should see below info in month dropdown")
+    public void user_should_see_below_info_in_month_dropdown(List<String> expectedList) {
+        Select select = new Select(dropDownPage.monthDropDown);
+        List<WebElement> actualMonthAsWebElement = select.getOptions();
+        List<String> actualMonthsAsString = new ArrayList<>();
+
+        for (WebElement each:actualMonthAsWebElement) {
+            actualMonthsAsString.add(each.getText());
+        }
+
+      Assert.assertEquals(expectedList,actualMonthsAsString);
+
+
+    }
+
 
 
 
--- src/test/resources/features/dataTable.feature
+++ src/test/resources/features/dataTable.feature
@@ -12,10 +12,29 @@ Feature: Examples of Cucumber data table implementations
       | Mango      |
       | Tomato     |
       | Cucumber   |
-  @wip
+
     Scenario: Library login example using data table
       Given user is on login page of library app
       When  user enters username and password as below
       |username|librarian13@library|
       |password| 9rf6axdD         |
       Then user should see title is Library
+
+  @wip
+  Scenario: User should be able to see all 12 months in months
+  dropdown
+    Given User is on the dropdowns page of practice tool
+    Then User should see below info in month dropdown
+
+      | January   |
+      | February  |
+      | March     |
+      | April     |
+      | May       |
+      | June      |
+      | July      |
+      | August    |
+      | September |
+      | October   |
+      | November  |
+      | December  |
