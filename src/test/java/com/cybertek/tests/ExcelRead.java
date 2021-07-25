578920bf02ce766c34150bcc3705f28081fc5f76
Orkhan <jkrogers222@gmail.com>
2021-07-25 11:33:19 -0400

    reading from excel index start from 0

--- src/test/java/com/cybertek/tests/ExcelRead.java
+++ src/test/java/com/cybertek/tests/ExcelRead.java
@@ -1,6 +1,11 @@
 package com.cybertek.tests;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ExcelRead {
     /*
@@ -8,7 +13,23 @@ public class ExcelRead {
      */
 
     @Test
-    public void read_from_excel_file() {
+    public void read_from_excel_file() throws IOException {
+        String path = "SampleData.xlsx";
+
+        // to read from excel we need to load it to FileInputStream
+        FileInputStream fileInputStream = new FileInputStream(path);
+        //workbook>sheet>row>cell
+
+        // <1> Create a workbook
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+
+        // <2> We need to get specific sheet from currently opened workbook
+        XSSFSheet sheet = workbook.getSheet("Employees");
+
+        //<3> Select row and cell
+        //Print out Steven;s cell
+        //Indexes start from 0
+        sheet.getRow(1).getCell(0);
 
 
 
